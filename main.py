a27d1d898ad8fba74861c60ee3306353ef8f738b
Andrzej Dąbrowski <newaadd@gmail.com>
2018-11-22 13:44:47 +0100

    Add hat drawing

--- main.py
+++ main.py
@@ -3,57 +3,52 @@ import numpy as np
 import cv2
 import sys
 
+def init(cascPath,hatPath):
+    faceCascade = cv2.CascadeClassifier(cascPath) 
+    hatImg = cv2.imread("hat.png", cv2.IMREAD_UNCHANGED) #read hat image with alpha channel
+    video_capture = cv2.VideoCapture(0) #grab the camera 
+    return faceCascade, hatImg, video_capture
+
+
 #here goes the main logic
 def main():
-    
-    cascPath = "cascades/haarcascade_frontalface_default.xml"
-    smilePath = "cascades/smile.xml"
-    faceCascade = cv2.CascadeClassifier(cascPath)
-    smileCascade = cv2.CascadeClassifier(smilePath)
-    video_capture = cv2.VideoCapture(0) #grab the camera by the handler
+    faceCascade, hatImg, video_capture = init(\
+        "cascades/haarcascade_frontalface_default.xml",\
+        "hat.png")
     
     #main loop
     while True:
         ret, frame = video_capture.read() #read input from webcamera
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+        
         faces, num_of_faces = faceCascade.detectMultiScale2(
             gray,
             scaleFactor=1.1,
             minNeighbors=5,
-            minSize=(30, 30),
-           # flags=cv2.CV_HAAR_SCALE_IMAGE
+            minSize=(30, 30)
         )
         print(faces)
-        # Draw a rectangle around the faces
         for (x1, y1, w1, h1) in faces:
-            print("x:", x1)
-            face = gray[y1 : y1+h1, x1 : x1 + w1]            
-            smiles = smileCascade.detectMultiScale(
-                face,
-                scaleFactor = 3.5,
-                minNeighbors=5,
-               # minSize=(15,15)
-            ) 
-            print(smiles)
-            #draw a green rectangle around every face
-            cv2.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (0, 255, 0), 2)
-            for (x, y, w, h) in smiles:
-                #draw a yellow rectangle around every smile
-                cv2.rectangle(frame, (x1+x, y1+y), (x1+x+w, y1+y+h), (0, 255, 255), 2)
-
+            scaled = cv2.resize(hatImg, (0,0), fx=w1/75,fy=w1/75)
+            alpha_s = scaled[:, :, 3] / 255.0
+            alpha_l = 1.0 - alpha_s
+            y_shift = h1//2 - h1//8
+            x_shift = -w1//6
+            try:
+                for c in range(0, 3):
+                    frame[y1 - scaled.shape[0] + y_shift : y1 + y_shift, x1 + x_shift: x1 + scaled.shape[1] + x_shift, c] = \
+                    (alpha_s * scaled[:, :, c] + alpha_l * \
+                    frame[y1 - scaled.shape[0] + y_shift: y1 + y_shift,\
+                    x1 + x_shift : x1 + scaled.shape[1] + x_shift, c])
+            except ValueError:
+                print("ValueError")
         # Display the resulting frame
         cv2.imshow('Video', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     video_capture.release()
     cv2.destroyAllWindows()
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
